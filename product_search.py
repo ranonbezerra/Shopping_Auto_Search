ecaebca1a66820dbd8fc1cdfd35b4293243400a2
ranonbezerra <ranonbezerra@hotmail.com>
2023-12-26 18:12:47 -0300

    Addition of docstrings to the product_search.py file

--- product_search.py
+++ product_search.py
@@ -17,6 +17,16 @@ class ProductSearch():
         self.NUMBER_OF_SEARCH_WEBSITES = 1
         
     def shopping_list_search(self):
+        ''' Execute the list search on Google Shopping
+
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
 
         self.open_window()
 
@@ -35,21 +45,76 @@ class ProductSearch():
                     self.export_product_sheet()
                 
     def xlsx_writer(self, search_website_number):
+        ''' Create the file where the search results will be inserted later 
+
+        Parameters
+        ----------
+        search_website_number : Intenger
+            Index of the website being searched in the websites list
+        
+        Returns
+        -------
+        Pandas Object
+            Pandas Object with data regarding the created file
+        '''
         return pd.ExcelWriter(self.search_results_filename[search_website_number] ,engine='xlsxwriter')
 
     def open_window(self):
+        ''' Open a Google Chrome window 
+
+        Parameters
+        ----------
+        search_website_number : Intenger
+            Index of the website being searched in the websites list
+        
+        Returns
+        -------
+        None
+        '''
+
         self.chrome_window = webdriver.Chrome()
         self.chrome_window.maximize_window()
 
     def open_search_website(self, search_website_number):
+        ''' Open a search website in Google Chrome window previously opened 
+
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
         self.chrome_window.get(self.search_websites[search_website_number])
         sleep(2)
 
     def search_product(self):
+        ''' Execute search of product in the Google homepage 
+
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
+        
         self.chrome_window.find_element('xpath','/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea').send_keys(self.product_on_search, Keys.ENTER)
         sleep(2)
 
     def access_shopping_page(self):
+        ''' Loads, after the search, the Google Shopping page 
+
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        '''
         buttons_list = self.chrome_window.find_elements(By.LINK_TEXT,'Shopping')
 
         for button in buttons_list:
@@ -58,6 +123,18 @@ class ProductSearch():
         sleep(2)
 
     def get_search_results(self):
+        ''' Extract the results, accordling with the parameters given in the "products.xlsx", 
+        from the Google Shopping page 
+
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        Tuple
+            Tuple with two dictionaries of each kind of result list
+        '''
 
         sponsored_results = self.get_sponsored_results()
 
@@ -66,21 +143,16 @@ class ProductSearch():
         return (sponsored_results, other_matches)
 
     def get_sponsored_results(self):
-        ''' Obtém os dados da lista dos Resultados Patrocinados do Google Shopping
+        ''' Get the data from the "Sponsored Results" list of Google Shopping
 
         Parameters
         ----------
-        janela : WebDriver object
-            Janela do Google Chrome controlado pelo WebDriver
-        prod : str
-            Dicionário que será pesquisado no site outras_correspondencias
-        db : Pandas DataBase obj
-            Base de dados do Pandas extraída de arquivo base (buscas.xlsx)
+        None
         
         Returns
         -------
         dict
-            Dicionário gerado a partir dos dados obtidos
+            DataFrame Object from Pandas generated with the data obtained
         '''
         
         print('Starting to analyse values on the Sponsored List for {}'.format(self.product_on_search))
@@ -110,6 +182,17 @@ class ProductSearch():
             return dataframe_to_return
                     
     def get_resulting_webpages(self):
+        ''' Select which list from the Google Shopping to get the data
+
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        Selenium Object
+            Selenium object with information regarding the list of results to be extracted 
+        '''
 
         if self.scrapping_location == 'Sponsored Results':
             return self.chrome_window.find_elements('class name','KZmu8e')
@@ -118,6 +201,18 @@ class ProductSearch():
             return self.chrome_window.find_elements('class name','i0X6df')
 
     def get_name_from_webpage(self, result):
+        ''' Get the name of the product from the websearch
+
+        Parameters
+        ----------
+        result : Selenium Object
+            Data from the website containing the price information of one of the products results.
+        
+        Returns
+        -------
+        String
+            String with the name of the product obtained from the search 
+        '''
         
         if self.scrapping_location == 'Sponsored Results':
             name_on_webpage = result.find_element('class name','ljqwrc')
@@ -127,10 +222,35 @@ class ProductSearch():
         elif self.scrapping_location == 'Other Matches':
             return result.find_element('class name','tAxDx').text
 
-    def treat_name_from_webpage(self, name):
-        return name.lower()
+    def treat_name_from_webpage(self, product_title):
+        '''Treat the name string on the search result 
+
+        Parameters
+        ----------
+        name : String
+            String of the title from the product search
+        
+        Returns
+        -------
+        String
+            Product title treated.
+        '''
+        
+        return product_title.lower()
 
     def get_price_from_webpage(self, result):
+        '''Extract the price string of on of the search results obtained from the website 
+
+        Parameters
+        ----------
+        result : Selenium Object
+            Data from the website containing the price information of one of the products results.
+        
+        Returns
+        -------
+        String
+            Product price in float format.
+        '''
             
         if self.scrapping_location == 'Sponsored Results':
             return result.find_element('class name','T14wmb').find_element('tag name','b').text
@@ -150,14 +270,26 @@ class ProductSearch():
         -------
         Float
             Product price in float format.
-            
         '''
+
         price = price[:price.find(',')+3]
         price = price.replace('R$','').replace(' ','').replace('.','').replace(',','.')
         price = re.sub("[^\d\.]", "", price)
         return float(price)
     
     def get_link_from_webpage(self, result):
+        '''Extract the price string of on of the search results obtained from the website 
+
+        Parameters
+        ----------
+        result : Selenium Object
+            Data from the website containing the link information of one of the products results.
+        
+        Returns
+        -------
+        String
+            String with the link of the product.
+        '''
             
         if self.scrapping_location == 'Sponsored Results':
 
@@ -252,15 +384,51 @@ class ProductSearch():
         self.product_dataframe = pd.DataFrame.from_dict(final_dataframe).sort_values(by=['Price'])
 
     def export_product_sheet(self):
+        '''Execute the extraction of the results into a sheet inside of a file in XLSX format
+
+        Parameters
+        ----------
+        None
+            
+        Returns
+        -------
+        None
+        '''
+
         sheetname = self.set_sheetname()
         self.product_dataframe.to_excel(self.search_results_file, sheet_name=sheetname, index=False)
 
     def set_sheetname(self):
+        '''Sets sheetname for the product being extracted
+
+        Parameters
+        ----------
+        None
+            
+        Returns
+        -------
+        String
+            String with the sheet name
+        '''
+
 
         sheetname = self.product_on_search + '_Google'
         return self.correct_sheetname_size(sheetname)
 
     def correct_sheetname_size(self, sheetname):
+        '''Treat sheetname string
+
+        Parameters
+        ----------
+        sheetname : String
+            String with the sheetname non treated
+            
+        Returns
+        -------
+        String
+            String with the sheet name treated
+        '''
+
 
         return sheetname[:31] if len(sheetname) > 31 else sheetname
 
